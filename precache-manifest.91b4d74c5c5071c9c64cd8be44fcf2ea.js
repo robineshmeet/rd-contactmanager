11dc7600f8a3e107082ccecd1c02cbeb1cbd5429
robineshmeet <robineshmeet@gmail.com>
2020-05-16 09:25:13 +0530

    Updates

---
+++ precache-manifest.91b4d74c5c5071c9c64cd8be44fcf2ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e558f2ab092fd0023a659b8c9d7507",
+    "url": "/rd-contactmanager/index.html"
+  },
+  {
+    "revision": "073fb83c36aeab4ef71c",
+    "url": "/rd-contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0f207eb3cbbf23b408a0",
+    "url": "/rd-contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "073fb83c36aeab4ef71c",
+    "url": "/rd-contactmanager/static/js/2.7bff490b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rd-contactmanager/static/js/2.7bff490b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f207eb3cbbf23b408a0",
+    "url": "/rd-contactmanager/static/js/main.f6fdec51.chunk.js"
+  },
+  {
+    "revision": "d040b96f26c148219e46",
+    "url": "/rd-contactmanager/static/js/runtime-main.958e38c6.js"
+  }
+]);
